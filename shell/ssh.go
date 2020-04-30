6f9a404daa904b17473ec204569e2e2ae65ca15f
meifakun <runner.mei@gmail.com>
2020-04-30 19:55:45 +0800

    fix ssh

--- shell/ssh.go
+++ shell/ssh.go
@@ -9,13 +9,27 @@ import (
 	"golang.org/x/crypto/ssh"
 )
 
+// SupportedCiphers xx
 var SupportedCiphers = GetSupportedCiphers()
+var SupportedKeyExchanges = GetKeyExchanges()
 
+// GetSupportedCiphers xx
 func GetSupportedCiphers() []string {
 	config := &ssh.ClientConfig{}
 	config.SetDefaults()
 
-	for _, cipher := range []string{"aes128-gcm@openssh.com", "aes128-cbc", "3des-cbc"} {
+	for _, cipher := range []string{
+		"aes128-cbc",
+		"aes128-ctr",
+		"aes192-ctr",
+		"aes256-ctr",
+		"aes128-gcm@openssh.com",
+		"chacha20-poly1305@openssh.com",
+		"arcfour256",
+		"arcfour128",
+		"arcfour",
+		"3des-cbc",
+	} {
 		found := false
 		for _, defaultCipher := range config.Ciphers {
 			if cipher == defaultCipher {
@@ -28,14 +42,37 @@ func GetSupportedCiphers() []string {
 			config.Ciphers = append(config.Ciphers, cipher)
 		}
 	}
+
 	return config.Ciphers
 }
-
-var SupportedKeyExchanges = GetKeyExchanges()
 
 func GetKeyExchanges() []string {
 	config := &ssh.ClientConfig{}
 	config.SetDefaults()
+
+	for _, keyAlg := range []string{
+		"diffie-hellman-group1-sha1",
+		"diffie-hellman-group14-sha1",
+		"ecdh-sha2-nistp256",
+		"ecdh-sha2-nistp384",
+		"ecdh-sha2-nistp521",
+		"curve25519-sha256@libssh.org",
+		"diffie-hellman-group-exchange-sha1",
+		"diffie-hellman-group-exchange-sha256",
+	} {
+		found := false
+		for _, defaultKeyAlg := range config.KeyExchanges {
+			if keyAlg == defaultKeyAlg {
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			config.KeyExchanges = append(config.KeyExchanges, keyAlg)
+		}
+	}
+
 	return config.KeyExchanges
 }
 
