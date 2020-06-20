85eb4eea09ce0470b2cf84b134667d8f0323fe1f
meifakun <runner.mei@gmail.com>
2020-06-20 20:35:21 +0800

    feat(authn): fix AAA authorization failed

--- shell/interactive.go
+++ shell/interactive.go
@@ -52,6 +52,9 @@ var (
 	defaultPermissionPrompts = [][]byte{
 		[]byte("Invalid input detected at '^' marker"),
 		[]byte("Error: Too many parameters found at '^' position."),
+		[]byte("Error: AAA authorization failed"),
+		[]byte("Error:AAA authorization failed"),
+		[]byte("Command authorization failed."),
 	}
 )
 
