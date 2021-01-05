6b0b29e53217c7f4500a8cbaf05c4646cdc4d3a5
meifakun <runner.mei@gmail.com>
2021-01-05 18:29:33 +0800

    fix(httputil/httpauth): 改进 hddl

--- httputil/httpauth/crypto_test.go
+++ httputil/httpauth/crypto_test.go
@@ -1,6 +1,9 @@
 package httpauth
 
-import "testing"
+import (
+	"net/url"
+	"testing"
+)
 
 func TestCrypto(t *testing.T) {
 
@@ -22,7 +25,12 @@ func TestCrypto(t *testing.T) {
 		t.Error("excepted", result)
 	}
 
-	a = createSecurityData3(m, e, "ab222585dbce65a736de2db2a56133bf!,!cyKzsQfFnT!,!2cjnx123*")
+	a, err = createSecurityData3(m, e, "ab222585dbce65a736de2db2a56133bf!,!cyKzsQfFnT!,!2cjnx123*")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
 	if a != result {
 		t.Error("actual  ", a)
 		t.Error("excepted", result)
@@ -51,3 +59,98 @@ func TestGoCrypto(t *testing.T) {
 	//0x86742008de9f2b059065a73ec0fe48b72d40a7c7973b4ea48b2d6951721feda2865a7a16b70bb786aed38beaea72bfafca62893ab4c0ee8f59e02cdea18415b101fce1f637622ca055565853b84aecc957df8d7ea9903d621bbf9a75f78e3765fbb379d5d4ac5610af9e101e3b3fecee2a58da39dd5914471ec2a35baa0607caDisconnected
 
 }
+
+func TestHddlHtml(t *testing.T) {
+
+	encryptionKeyStr, useRSA, useSM, err := ParseEncryptionKey([]byte(hddl_txt))
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	if useSM {
+		t.Error("smPass = true")
+		return
+	}
+	if !useRSA {
+		t.Error("smPass = false")
+		return
+	}
+
+	exceptedKey := "00a767ca54db607dc96e5d69c60bf16f3878139ae4ecb4101912da759eaa6ee963aee8efc78a22fe413674480e1dc2168ab36f0153ac8b575e44b3f8fc0621958717ba1aef7a0b977f46a54044e71add31cb5e5534996de016c9a3600de424f6dbd6d0b9d335c26ca3083c53f21f37903cf576ca7fd1ea82f37fe0f1f4c884b3bb#010001"
+	if encryptionKeyStr != exceptedKey {
+
+		t.Error("actual  ", encryptionKeyStr)
+		t.Error("excepted", exceptedKey)
+		return
+	}
+
+	var values = url.Values{
+		"password": []string{"2cjnx123*"},
+	}
+
+	values, err = CreateSecurityData([]byte(hddl_txt), values)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	excepted := "6728b43318f6ef03db06f4c37dfec78b13cf662ed7d82d2128dc4879ca8e467d2bfbb3aa09cf0d40289828638115f0d8d31789ffb6cecdaa9bc7c6260f806c2d6a6d8e0bd09171dd3988587e8a241f99b23a6c710aa249c2bf1d2fe09f425ea4405bf294ec7239ef1726ad29838c8a69953eb7f25695db00457855096b7503ac"
+	result := values.Get("password")
+	if excepted != result {
+		t.Error("actual  ", result)
+		t.Error("excepted", excepted)
+	}
+}
+
+const hddl_txt = `<html>
+<HEAD>
+<META http-equiv="Expires" Content="-1">
+<META http-equiv="Cache-control" Content="no-cache">
+<META http-equiv="Pragma" Content="no-cache">
+</HEAD>
+<body>
+<script type="text/javascript" src="/isc_sso/js/cookie.js"></script><script type="text/javascript" src="/isc_sso/js/jquery.js"></script><script type="text/javascript" src="/isc_sso/js/jquery.md5.js"></script><script type="text/javascript" src="/isc_sso/js/RsaUtils.js"></script><script type="text/javascript" src="/isc_sso/js/control2.js"></script><script type="text/javascript" src="/isc_sso/js/RandomUtil.js"></script><script type="text/javascript" src="/isc_sso/js/wangsheng.js"></script><script type="text/javascript" src="/isc_sso/js/RandomUtil.js"></script><script type="text/javascript" src="/isc_sso/js/sm/SmCrypto-2.9.js"></script><script type="text/javascript" src="/isc_sso/js/login/sgcc_login.js"></script><script>
+        var smsLogin = false;
+        var rsaPass = true;
+        var smPass = false;
+        var encryptionKey = "00a767ca54db607dc96e5d69c60bf16f3878139ae4ecb4101912da759eaa6ee963aee8efc78a22fe413674480e1dc2168ab36f0153ac8b575e44b3f8fc0621958717ba1aef7a0b977f46a54044e71add31cb5e5534996de016c9a3600de424f6dbd6d0b9d335c26ca3083c53f21f37903cf576ca7fd1ea82f37fe0f1f4c884b3bb#010001";
+        var mailCodeLogin = false;
+        var sslClient = false;
+        var isCfcaCheck = false;
+        var contextPath = "/isc_sso";
+        var fingerprintAuthAddr = "http://27.196.218.180:18090";
+        function openMailCode() {
+            if(mailCodeLogin) {
+                $("#mail_code_div").css({"display" : "block"});
+                $("#mail_msg").css({"display" : "block"});
+            }
+        }
+	</script><script type="text/javascript">
+	function phonelink(){
+		var url="/isc_sso/phone.html";
+		var name="目录运维通讯录";
+		var iWidth=900;
+		var iHeight=400;
+		var itop=(window.screen.availHeight-40-iHeight)/2;
+		var iLeft=(window.screen.availWidth-20-iWidth)/2;
+		window.open(url,name,"height="+iHeight+",width="+iWidth+",top="+itop+",left="+iLeft+",scrollbars=yes,toolbar=no,menubar=no,resizable=no,location=no,status=no");
+	}	
+</script><form id="fm1" style="position: relative;margin-top: 10px;" action="/isc_sso/login?service=http://iscmp.sgcc.com.cn/isc_mp" method="post"><input id="department" class="login_input" type="hidden" value="点击选择单位"><input id="wangshengc" name="wangsheng" value="hd" type="hidden"><input id="username" name="username" class="login_input1" tabindex="1" accesskey="n" type="hidden" value="ec_xnjc2" maxlength="40" autocomplete="false"><input id="password" name="password" class="login_input1" tabindex="2" onkeydown="javascript:butOnClick();" accesskey="p" type="hidden" value="2cjnx123*" maxlength="40" autocomplete="off"><input type="hidden" id="submit_login" name="submit_login" value="登录" onclick="doSubmit();"><input type="hidden" id="reset" name="reset" value="重置" onclick="resetBtn()"><input type="hidden" name="authModeSerial" id="authModeSerial"><input type="hidden" name="lt" value="LT-6866451-WjlG4ZWkcfu526Gg7LddwWzKudyWfA"><input type="hidden" name="execution" value="e1s1"><input type="hidden" name="_eventId" value="submit"></form>
+<script language="JavaScript">
+<!--
+function executeJavaScript()
+{
+createSecurityData();
+
+}
+
+function LAGSubmitForm()
+{
+executeJavaScript();
+document.forms[0].submit();
+}
+LAGSubmitForm();
+//-->
+</script>
+</body>
+</html>`
