491045f5c794f26afef6ada4b8bf6b174f75fb3b
meifakun <runner.mei@gmail.com>
2021-03-30 21:15:37 +0800

    fix(shell): add "found at '' position" to error list

--- shell/interactive.go
+++ shell/interactive.go
@@ -49,12 +49,13 @@ var (
 		[]byte("Login invalid"),
 		[]byte("Access denied"),
 		[]byte("Login failed"),
-		[]byte("Error:")}
+		[]byte("Error:"),
+		[]byte("found at '^' position"),
+	}
 	defaultPermissionPrompts = [][]byte{
 		[]byte("Invalid input detected at '^' marker"),
 		[]byte("Error: Too many parameters found at '^' position."),
-		[]byte("Error: AAA authorization failed"),
-		[]byte("Error:AAA authorization failed"),
+		[]byte("AAA authorization failed"),
 		[]byte("Command authorization failed."),
 		[]byte("Unrecognized command found"),
 	}
